944eff0a768784afa6d2ac371d1ee828739d9a8c
unknown <faten.sroor@gmail.com>
2018-03-29 11:47:48 +0300

    withdraw from many atm's

--- atm.py
+++ atm.py
@@ -1,38 +1,48 @@
-totalMoney = 50000
+class Atm():
+	def __init__(self, balance, bank_name):
+		self.balance = balance
+		self.bank_name = bank_name
+	
+	def withdraw(self, request):
+		balance = self.balance
+		print ("welcome to "+self.bank_name+"your current balance is: ",self.balance)
+		request = int (input("enter the amount of money you want to withdraw")) 
+		if request<0:
+			print("invalid number")
+		elif request > balance:
+			print("you don't have enough balance")
+		else:
+			self.balance =self.balance-request
+			print ("you are going to have", request, "your current balance now is:",self.balance) 
+			
+			while request <self.balance:
+				if request %100==0:
+					x= request//100
+					for i in range (0,x):
+						print("given 100")
+					request = request%100
+				if request%100>0:
+					x= request//100
+					for i in range(0,x):
+						print("given 100")
+					request = request%100
+					if request//50==1:
+						print("given 50")
+						request = request%50
+					if request!=0:
+						#print "given 50"
+						#request=request%50
+						x=request//10
+						for i in range (0,x):
+							print("given 10")
+						request =request%10
+						if request>=5:
+							print("given 5")
+							request = request-5
+							print ("given",request)
+						elif request !=0:
+							print ("given",request) 
+				request=0
+			return balance
+			print ("your balance now is:",self.balance)
 
-request = input("enter the amount of money you want to withdraw") 
-
-if request<0:
-	print "invalid number"
-elif request > totalMoney:
-	print "you don't have enough balance"
-else:
-	print ("you are going to have", request)
-	while request <totalMoney:
-		if request %100==0:
-			x= request/100
-			for i in range (0,x):
-				print "given 100"
-			request = request%100
-		if request%100>0:
-			x= request/100
-			for i in range(0,x):
-				print "given 100"
-			request = request%100
-			if request/50==1:
-				print "given 50"
-				request = request%50
-			if request!=0:
-				#print "given 50"
-				#request=request%50
-				x=request/10
-				for i in range (0,x):
-					print "given 10"
-				request =request%10
-				if request>=5:
-					print "given 5"
-					request = request-5
-					print ("given",request)
-				elif request !=0:
-					print ("given",request) 
-		request=0
---
+++ atm_refactor.py
@@ -0,0 +1,8 @@
+import Atm
+balance1 = 50000
+balance2 = 100000
+
+atm1 = Atm.Atm(balance1, "smart bank")
+atm2 = Atm.Atm(balance2, "whatever bank")
+
+atm1.withdraw(6000)
